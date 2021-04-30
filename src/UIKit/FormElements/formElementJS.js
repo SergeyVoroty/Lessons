ddb5fed32faa81f1ac7c5297e98a363e960bcab1
Sergey_Voroty <sergei.vorot22@gmail.com>
2021-05-01 00:11:42 +0300

    Mackbook was die, Added 50x50 working checkbox

--- src/UIKit/FormElements/formElementJS.js
+++ src/UIKit/FormElements/formElementJS.js
@@ -1,15 +1,34 @@
 import * as $ from 'jquery'
 let isExpanded = false
-$('.expandableCheckboxListContainer_expend_more_icon').on('click', function (){
-    if (!isExpanded) {
-        $('.expandableCheckboxListItemsContainer').addClass('expandableCheckboxListItemsContainer_expanded')
-        $('.expandableCheckboxListContainer_expend_more_icon').addClass('expandableCheckboxListContainer_expend_more_icon_expanded')
-        isExpanded = true
-    } else {
-        $('.expandableCheckboxListItemsContainer').removeClass('expandableCheckboxListItemsContainer_expanded')
-        $('.expandableCheckboxListContainer_expend_more_icon').removeClass('expandableCheckboxListContainer_expend_more_icon_expanded')
-        isExpanded = false
-    }
-    console.log('Expend is clicked')
+let expandedFirst = false
+if (!expandedFirst) {
+    $('.expandableCheckboxListContainer').on('click', function () {
+        if (!isExpanded) {
+            $('.expandableCheckboxListItemsContainer').addClass('expandableCheckboxListItemsContainer_expanded')
+            $('.expandableCheckboxListContainer_expend_more_icon').addClass('expandableCheckboxListContainer_expend_more_icon_expanded')
+            let isChecked = false
+            var items = $('.expandableCheckboxListItemsContainer_expanded').children()
+            console.log('checked', items)
+            $.each(items, function (index, element) {
+                let isChecked = false
+                $(element).on('click', function () {
+                    if (!isChecked) {
+                        $(element).addClass('expandableCheckboxList_item_checked')
+                        isChecked = true
+                    } else {
+                        $(element).removeClass('expandableCheckboxList_item_checked')
+                        isChecked = false
+                    }
+                })
+            })
+            isExpanded = true
+        } else {
+            $('.expandableCheckboxListItemsContainer').removeClass('expandableCheckboxListItemsContainer_expanded')
+            $('.expandableCheckboxListContainer_expend_more_icon').removeClass('expandableCheckboxListContainer_expend_more_icon_expanded')
+            isExpanded = false
+        }
+        console.log('Expend is clicked')
 
-})
+    })
+
+}
